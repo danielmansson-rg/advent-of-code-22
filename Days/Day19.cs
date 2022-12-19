78d103b150b847f761edae8db82989a9c445098a
Daniel Månsson <daniel.mansson@resolutiongames.com>
2022-12-19 09:43:24 +0100

    Day 19 WIP

--- Days/Day19.cs
+++ Days/Day19.cs
@@ -8,7 +8,7 @@ Blueprint 2:  Each ore robot costs 2 ore.  Each clay robot costs 3 ore.  Each ob
             public int id;
             public Dictionary<string, Dictionary<string, int>> costs;
         }
-        
+
         List<Blueprint> Transform(string raw) {
             return raw
                 .Replace(":", ".")
@@ -16,37 +16,83 @@ Blueprint 2:  Each ore robot costs 2 ore.  Each clay robot costs 3 ore.  Each ob
                 .Split("\n")
                 .Select(l => {
                     var parts = l.Split(".");
-                    
-                    var part = parts
+
+                    var allCosts = parts
+                        .SkipLast(1)
                         .TakeLast(4)
                         .Select(p => {
-                        var s = part.Split(" ")
-                            .ToList();
-                        
-                        var start = s.IndexOf("costs") + 1;
-                        var robotType = s[start - 2];
-                    
-                        for(int i = start; i < s.Count;) {
-                            var type = s[i + 1];
-                            switch(type) {
-                                case "ore":
+                            var s = p.Split(" ")
+                                .ToList();
+                            
+                            var start = s.IndexOf("costs") + 1;
+                            var robotType = s[start - 3];
+                            var costs = new Dictionary<string, int>();
+                            
+                            for(int i = start; i < s.Count; i += 2) {
+                                var type = s[i + 1];
+                                costs.Add(type, int.Parse(s[i]));
                             }
-                        }
-                    });
+                            return (robotType, costs);
+                        });
                     
                     var bp = new Blueprint() {
-                        id = int.Parse(parts[1]),
+                        id = int.Parse(parts[0].Split(" ")[1]),
+                        costs = allCosts.ToDictionary(v => v.robotType, v => v.costs)
                     };
 
-                    return new Blueprint();
+                    return bp;
                 })
                 .ToList();
         }
 
         public override object Solve1(string raw) {
-            var input = Transform(raw);
+            var blueprints = Transform(raw);
 
-            return -1;
+            List<int> qualityLevels = new();
+            
+            foreach(Blueprint blueprint in blueprints) {
+                Dictionary<string, int> res = new() {
+                    {"ore", 0},
+                    {"clay", 0},
+                    {"obsidian", 0},
+                    {"geode", 0},
+                };
+                Dictionary<string, int> robots = new() {
+                    {"ore", 1},
+                    {"clay", 0},
+                    {"obsidian", 0},
+                    {"geode", 0},
+                };
+                
+                for(int i = 0; i < 24; i++) {
+                    //start build
+                    var robotChoice = blueprint.costs
+                        .FirstOrDefault(c => c.Value
+                            .All(v => res[v.Key] >= v.Value));
+                    if(robotChoice.Key != null) {
+                        foreach(KeyValuePair<string,int> cost in robotChoice.Value) {
+                            res[cost.Key] -= cost.Value;
+                        }
+                    }
+
+                    //get resources
+                    foreach(KeyValuePair<string,int> robot in robots) {
+                        res[robot.Key] += robot.Value;
+                    }
+
+                    //build done
+                    if(robotChoice.Key != null) {
+                        robots[robotChoice.Key] += 1;
+                    }
+                }
+
+                int obsidian = res["obsidian"];
+                int qualityLevel = obsidian * blueprint.id;
+
+                qualityLevels.Add(qualityLevel);
+            }
+
+            return qualityLevels.Count;
         }
 
         public override object Solve2(string raw) {
